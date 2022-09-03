0ab93bdaf9cc38c4c94f0d801adae304d1dfadf9
Paul Brian Marcuelo <marcuelo1@gmail.com>
2022-09-03 17:36:50 +0800

    configured and setup sign in view

---
+++ src/app/features/auth/contexts/authContext.jsx
@@ -0,0 +1,93 @@
+import React, { createContext, Component } from 'react';
+import { postFetch } from '../../../core/network/fetchData';
+import SessionsManager from '../../../core/network/sessionsManager';
+
+export const AuthContext = createContext();
+
+class AuthContextProvider extends Component {
+  state = { 
+    
+  } 
+
+  constructor() {
+    super()
+    // var headers = SessionsManager.getHeaders()
+    // if (headers['access-token'] !== "" || headers['client'] !== "" || headers['uid'] !== ""){
+    //   window.location.href = `/`
+    // }
+  }
+
+  componentDidMount(){
+    
+  }
+
+  submitSignIn = () => {
+    var isValid = true
+
+    // text inputs
+    var textInputs = ["#email", "#password"]
+    for (let i = 0; i < textInputs.length; i++) {
+      const id = textInputs[i];
+      
+      if(document.querySelector(id).value === ""){
+        document.querySelector(id).classList.add('empty')
+        isValid = false
+        break
+      }else if(document.querySelector(id).name === "email") {
+        const validateEmail = (email) => {
+          return String(email)
+            .toLowerCase()
+            .match(
+              /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+        };
+      
+        if(validateEmail(document.querySelector(id).value) === null){
+          document.querySelector(id).classList.add('empty')
+          isValid = false
+          break
+        }else{
+          document.querySelector(id).classList.remove('empty')
+        }
+      }else{
+        document.querySelector(id).classList.remove('empty')
+      }
+    }
+
+    if(!isValid){
+      return false
+    }
+
+    // get form data
+    var data = {
+      email: document.querySelector("#email").value,
+      password: document.querySelector("#password").value,
+    }
+
+    var config = {
+      pathname: "/admin/admin_auth/sign_in",
+      data: data,
+      dataFunction: (data) => {
+        window.location.href = `/`
+      },
+      errorFunction: (error) => {
+      }
+    }
+    postFetch(config)
+  }
+
+  render() { 
+    const value = {
+      ...this.state,
+      submitSignIn: this.submitSignIn
+    }
+
+    return (
+      <AuthContext.Provider value={value}>
+        {this.props.children}
+      </AuthContext.Provider>
+    );
+  }
+}
+ 
+export default AuthContextProvider;
---
+++ src/app/features/auth/views/signinView.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AuthContextProvider, { AuthContext } from '../contexts/authContext';
+import '../stylesheets/signin.scss';
+
+const SignInView = () => {
+
+  return (
+    <AuthContextProvider>
+      <div id="signin-page">
+        <div className="custom-card signin-box">
+          <div className="form-container">
+            <h1>Welcome Back!</h1>
+            <form action="#">
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="email" className='form-control' />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password" className='form-control' />
+              </div>
+            </form>
+            <AuthContext.Consumer>{ context => {
+              const { submitSignIn } = context 
+
+              return (
+                <button className="btn btn-dark" onClick={submitSignIn}>Sign In</button>
+              )
+            }}</AuthContext.Consumer>
+          </div>
+        </div>
+      </div>
+    </AuthContextProvider>
+  )
+}
+
+export default SignInView;
