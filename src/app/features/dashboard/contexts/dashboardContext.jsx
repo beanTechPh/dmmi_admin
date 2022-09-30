300185267fbb7e05b9f48bc599c73d4b5cca5d99
Paul Brian Marcuelo <marcuelo1@gmail.com>
2022-10-01 06:14:37 +0800

    added dashboard context

--- src/app/features/dashboard/contexts/dashboardContext.jsx
+++ src/app/features/dashboard/contexts/dashboardContext.jsx
@@ -6,9 +6,6 @@ export const DashboardContext = createContext();
 
 class DashboardContextProvider extends Component {
   state = { 
-    admins: [],
-    adminsPage: 1,
-    adminsTotalPage: 1,
    } 
 
   getData (config) {
@@ -20,17 +17,12 @@ class DashboardContextProvider extends Component {
     var keyword = config.keyword === undefined ? "" : config.keyword
 
     config = {
-      pathname: "/admin/admins",
+      pathname: "/admin/dashboard",
       data: {
         page: page,
         keyword: keyword,
       },
       dataFunction: (data) => {
-        var admins = Admin.rawDataToAdmins(data['admins'])
-        var adminsPage = data['pagination']['page']
-        var adminsTotalPage = data['pagination']['total_page']
-        
-        this.setState({ admins, adminsPage, adminsTotalPage })
       },
       errorFunction: (error) => {
       }
@@ -40,7 +32,7 @@ class DashboardContextProvider extends Component {
   }
 
   componentDidMount(){
-    // this.getData();
+    this.getData();
   }
 
   query = (config) => {
